bf881123fb515061d387ca5867fde7f677f8da4f
Lasath Fernando <kde@lasath.org>
2014-10-01 22:56:11 +1000

    Create prepared spells list

---
+++ prep_spells.js
@@ -0,0 +1,52 @@
+var prep_spells = {
+    // 8th
+    "Destruction": 1,
+    "Maze": 1,
+    "Power Word Stun": 1,
+
+    // 7th
+    "Chain Lightning": 1,
+    "Heal": 1,
+    "Scrying, Greater": 0,
+
+    // 6th
+    "True Seeing": 1,
+    "Raise Dead": 0,
+    "Dispel Magic, Greater": 2,
+
+    // 5th
+    "Break Enchantment": 1,
+    "Teleport": 1,
+    "Ill Omen": {
+        "prep": 1,
+        "quickened": true,
+    },
+
+    // 4th
+    "Threefold Aspect": 1,
+    "Dimension Door": 4,
+
+    // 3rd
+    "Remove Blindness/Deafness": 1,
+    "Remove Curse": 1,
+    "Tounges": 1,
+    "Dispel Magic": 2,
+    "Arcne Sight": 2,
+    "Barrow Haze": 1,
+    "Locate Object": 1,
+
+    // 2nd
+    "See Invisibility": 2,
+    "Glitterdust": 2,
+    "Squeeze": 2,
+    "Staggering Fall": 1,
+    "Status": 2,
+    "Fog Cloud": 1,
+
+    // 1st
+    "Air Bubble": 1,
+    "Comprehend Languages": 1,
+    "Dancing Lantern": 1,
+    "Identify": 2,
+    "Reduce Person": 1,
+}
