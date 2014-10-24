b2364454b98b7b579f2c44f7409295e68b2f2b8f
Lasath Fernando <kde@lasath.org>
2014-10-24 19:04:09 +1100

    Update prepared spells list

--- prep_spells.js
+++ prep_spells.js
@@ -1,41 +1,42 @@
 var prep_spells = {
-    // 8th
-    "Destruction": 1,
-    "Maze": 1,
+    // 8th (3)
+    "Destruction": 1, // Quickened, through Rod
+    "Maze": 1, // Quickened, through Rod
     "Power Word Stun": 1,
 
-    // 7th
+    // 7th (4)
     "Chain Lightning": 1,
     "Heal": 1,
     "Scrying, Greater": 0,
 
-    // 6th
+    // 6th (5)
     "True Seeing": 1,
     "Raise Dead": 0,
     "Dispel Magic, Greater": 2,
 
-    // 5th
+    // 5th (6)
     "Break Enchantment": 1,
-    "Teleport": 1,
+    "Teleport": 0,
     "Ill Omen": {
         "prep": 4,
         "quickened": true,
     },
 
-    // 4th
+    // 4th (6)
     "Threefold Aspect": 1,
     "Dimension Door": 4,
+    "Dimension Door": 1, // Quickened
 
-    // 3rd
+    // 3rd (6)
     "Remove Blindness/Deafness": 1,
     "Remove Curse": 1,
-    "Tounges": 1,
+    "Tounges": 0,
     "Dispel Magic": 2,
     "Arcne Sight": 2,
     "Barrow Haze": 1,
-    "Locate Object": 1,
+    "Locate Object": 0,
 
-    // 2nd
+    // 2nd (14)
     "See Invisibility": 2,
     "Glitterdust": 2,
     "Squeeze": 2,
@@ -43,10 +44,10 @@ var prep_spells = {
     "Status": 2,
     "Fog Cloud": 1,
 
-    // 1st
+    // 1st (7)
     "Air Bubble": 1,
     "Comprehend Languages": 1,
     "Dancing Lantern": 1,
     "Identify": 2,
-    "Reduce Person": 1,
+    "Reduce Person": 2,
 }
